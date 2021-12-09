d4a0cc50f34b1c13c6624e2b3b20f92a16e7104d
RenoDeCorte <renodecorte@gmail.com>
2021-12-08 16:31:00 -0800

    Added threading function + renamed file

---
+++ threads.py
@@ -0,0 +1,15 @@
+import threading
+
+
+def create_threads(list_of_ip, function):
+    """ Create a thread per ip, so that commands can be executed on each device concurrently """
+
+    threads = []
+
+    for ip in list_of_ip:
+        th = threading.Thread(target=function, args=(ip,))
+        th.start()     # start the thread
+        threads.append(th)
+
+    for thread in threads:
+        thread.join()  # Wait for all threads to terminate
